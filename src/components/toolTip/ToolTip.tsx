f37f4ce945e3e3a92a40a9d8e05ca6094ee978f3
DongxingWang <dongxingwang@zhongkehuizhou.com>
2025-08-07 15:26:51 +0800

    修改dock动画

--- src/components/toolTip/ToolTip.tsx
+++ src/components/toolTip/ToolTip.tsx
@@ -7,7 +7,7 @@ const ToolTip: FunctionalComponent<{ title: string }> = (
     return (
         <div class="group relative">
             {slots.default?.()}
-            <span class="absolute bottom-full left-1/2 mb-2 w-max -translate-x-1/2 px-3 py-1.5 text-sm text-white bg-gray-800 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-300 pointer-events-none z-10">
+            <span class="absolute bottom-12 left-1/2 mb-2 w-max -translate-x-1/2 px-3 py-1.5 text-sm text-white bg-gray-800 rounded opacity-0 group-hover:opacity-300 transition-opacity duration-300 delay-100 pointer-events-none z-10">
                 {props.title}
             </span>
         </div>
