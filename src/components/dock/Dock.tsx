d3f474b7aefb46f54a53d9f3a906fb476f5a3bcd
DongxingWang <dongxingwang@zhongkehuizhou.com>
2025-08-08 13:47:42 +0800

    修改样式

--- src/components/dock/Dock.tsx
+++ src/components/dock/Dock.tsx
@@ -16,12 +16,7 @@ const Dock = defineComponent(() => {
 
     return () => (
         <div class="fixed bottom-4 w-full flex justify-center pointer-events-none">
-            <div
-                class="flex bg-white/20 px-4 py-2 rounded-2xl shadow-xl space-x-4 pointer-events-auto"
-                style={{
-                    backdropFilter: 'blur(2px)',
-                }}
-            >
+            <div class="flex bg-white/20 px-4 py-2 rounded-2xl shadow-xl space-x-4 pointer-events-auto backdrop-blur-[2px]">
                 {map(dockItems.value, (item) => (
                     <DockItem
                         id={item.id}
